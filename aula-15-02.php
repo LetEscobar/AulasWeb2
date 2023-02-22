3bfb9ff4df9ad93b372b806fd539500c28b3d9db
Letícia Escobar <leticiaescobaraujo@gmail.com>
2023-02-22 17:26:43 -0300

    Aula - 15/12

---
+++ aula-15-02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>EXEMPLO DE SITE PHP</title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <?php
+            $escola = 'IFMS'
+        ?>
+        <h1>Página <?php echo $escola;?></h1>
+        <?php
+            for ($i = 1; $i <= 10; $i++){
+                echo '<li class = "list-group-item"> Aluno' . $i . '</li>'
+            }
+        ?>
+    </body>
+</html>
